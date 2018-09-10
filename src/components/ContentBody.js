3021176d5d1ca9017776f39e3ab45dffd28e0256
nguyenthe675 <nguyenthe675@gmail.com>
2018-09-10 17:37:34 +0700

    add task form

--- src/components/ContentBody.js
+++ src/components/ContentBody.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import uuidv1 from 'uuid/v1';
 import TaskList from './TaskList';
+import TaskForm from './TaskForm';
 
 class ContentBody extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      tasks: [] // id : unique, name, status
+      tasks: [], // id : unique, name, status
+      isShowForm : false
     }
   }
 
@@ -46,11 +48,20 @@ class ContentBody extends React.Component {
     console.log('Khởi tạo thành công');
   }
 
+  onShowForm = () => {
+    this.setState({
+      isShowForm : !this.state.isShowForm
+    });
+  }
+
   render() {
-    let { tasks } = this.state;
+    let { tasks, isShowForm } = this.state;
     return (
       <div className="row">
-        <div className="col-sm-12">
+        <div className={isShowForm ? "col-sm-4" : ""}>
+          { isShowForm ? <TaskForm></TaskForm> : ''}
+        </div>
+        <div className={ isShowForm ? "col-sm-8" : "col-sm-12"}>
           <div className="card">
             <div className="card-header">
               <h5 className="card-header-text">Danh sách công việc</h5>
@@ -58,7 +69,10 @@ class ContentBody extends React.Component {
             </div>
             <div className="card-block">
               <div className="form-group">
-                <button type="button" className="btn btn-primary waves-effect waves-light add">
+                <button type="button" 
+                className="btn btn-primary waves-effect waves-light add"
+                onClick={ this.onShowForm }
+                >
                   <i className="icofont icofont-plus m-r-5"></i>  Thêm mới
                 </button>&nbsp;
                 <button type="button" 
@@ -71,6 +85,7 @@ class ContentBody extends React.Component {
             </div>
           </div>
         </div>
+
       </div>
     );
   }
